85906f9140f02a6d83e3629452fcef1259b28348
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-09-04 19:32:00 +0530

    Add callback instead of anonymous function.
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- admin-tools-command.php
+++ admin-tools-command.php
@@ -20,11 +20,17 @@ if ( file_exists( $autoload ) ) {
 EE::add_command( 'admin-tools', 'Admin_Tools_Command' );
 
 
-EE::add_hook('before_invoke:admin-tools up', function ( $args, $assoc_args ) {
+EE::add_hook('before_invoke:admin-tools up', 'init_global_auth' );
 
+/**
+ * Initialize global auth if it's not present.
+ *
+ * @throws \EE\ExitException
+ */
+function init_global_auth() {
 	if ( ! is_array( EE::get_runner()->find_command_to_run( [ 'auth' ] ) ) ) {
-		EE::error( 'Auth command needs to be registered for admin_tools' );
+		EE::error( 'Auth command needs to be registered for mailhog' );
 	}
 
 	EE::run_command( [ 'auth', 'init' ] );
-});
+}
