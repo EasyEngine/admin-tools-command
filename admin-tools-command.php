60885f24e259717bee316472e34344add0fc0bed
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-09-05 15:18:05 +0530

    Add method from auth utils to initialize global admin tools
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- admin-tools-command.php
+++ admin-tools-command.php
@@ -13,24 +13,9 @@ if ( ! defined( 'ADMIN_TEMPLATE_ROOT' ) ) {
 }
 
 $autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
+
 if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
 EE::add_command( 'admin-tools', 'Admin_Tools_Command' );
-
-
-EE::add_hook('before_invoke:admin-tools up', 'init_global_auth' );
-
-/**
- * Initialize global auth if it's not present.
- *
- * @throws \EE\ExitException
- */
-function init_global_auth() {
-	if ( ! is_array( EE::get_runner()->find_command_to_run( [ 'auth' ] ) ) ) {
-		EE::error( 'Auth command needs to be registered for mailhog' );
-	}
-
-	EE::run_command( [ 'auth', 'init' ] );
-}
