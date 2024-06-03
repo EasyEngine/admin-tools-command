8e65aca4b3705725406ba9ac3311e2616164a42d
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-06-03 10:24:41 +0000

    Add Behat Testing
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

---
+++ features/admintools.feature
@@ -0,0 +1,9 @@
+Feature: EasyEngine Admin Tools
+
+  Scenario: Enable and disable admin tools with EasyEngine
+    Given I have installed EasyEngine if not installed
+    And I have created a WordPress site at "example.com"
+    When I run "ee admin-tools enable example.com"
+    Then I should be able to access "http://example.com/ee-admin/"
+    When I run "ee admin-tools disable example.com"
+    Then I should not be able to access "http://example.com/ee-admin/"
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,106 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    private $command_output;
+    private static $easyengine_installed = false;
+    private static $site_created = false;
+
+    /**
+     * @Given I have installed EasyEngine if not installed
+     */
+    public function iHaveInstalledEasyengineIfNotInstalled()
+    {
+        if (!self::$easyengine_installed) {
+            $install_command = 'wget -qO ee https://rt.cx/ee4 && sudo bash ee && sudo rm ee';
+            $install_output = shell_exec($install_command);
+            if (!file_exists('/usr/local/bin/ee')) {
+                throw new Exception("EasyEngine could not be installed.");
+            }
+            self::$easyengine_installed = true;
+        }
+    }
+
+    /**
+     * @Given I have created a WordPress site at :site
+     */
+    public function iHaveCreatedAWordpressSiteAtExampleCom()
+    {
+        if (!self::$site_created) {
+            $site = "example.com";
+            $site_directory = "/opt/easyengine/sites/$site";
+
+            if (!file_exists($site_directory)) {
+                $command = "ee site create $site --type=php";
+                $this->command_output = shell_exec($command);
+
+                $site_url = "http://$site";
+                sleep(5);
+                $site_accessible = $this->isSiteAccessible($site_url);
+
+                if (!$site_accessible) {
+                    throw new Exception("Failed to create WordPress site at $site. The site is not accessible.");
+                }
+            } else {
+                echo "WordPress site at $site already exists. Skipping site creation.";
+            }
+
+            self::$site_created = true;
+        }
+    }
+
+    /**
+     * Check if a site is accessible
+     *
+     * @param string $site_url The URL of the site to check
+     * @return bool True if the site is accessible, false otherwise
+     */
+    private function isSiteAccessible($site_url)
+    {
+        $headers = get_headers($site_url);
+        return $headers && strpos($headers[0], '200') !== false;
+    }
+
+    /**
+     * @When I run "ee admin-tools enable :site"
+     */
+    public function iRunEnableAdminTools($site)
+    {
+        $this->command_output = shell_exec("ee admin-tools enable $site");
+    }
+
+    /**
+     * @When I run "ee admin-tools disable :site"
+     */
+    public function iRunDisableAdminTools($site)
+    {
+        $this->command_output = shell_exec("ee admin-tools disable $site");
+    }
+
+    /**
+     * @Then I should be able to access :url
+     */
+    public function iShouldBeAbleToAccess($url)
+    {
+        sleep(5);
+        $headers = @get_headers($url);
+        if (!$headers || strpos($headers[0], '200') === false) {
+            throw new Exception("Failed to access $url. Expected 200 status code, but got: " . (is_array($headers) ? implode(', ', $headers) : 'No headers received'));
+        }
+    }
+
+    /**
+     * @Then I should not be able to access :url
+     */
+    public function iShouldNotBeAbleToAccess($url)
+    {
+        sleep(5);
+        $headers = @get_headers($url);
+        if (!$headers || strpos($headers[0], '403') === false) {
+            throw new Exception("Failed to access $url. Expected 403 status code, but got: " . (is_array($headers) ? implode(', ', $headers) : 'No headers received'));
+        }
+    }
+}
